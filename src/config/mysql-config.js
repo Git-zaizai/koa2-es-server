deebf4d9269c24a17b1e77f9cf0e49635e73b61d
Git-zaizai <2394276262@qq.com>
2022-05-07 17:01:05 +0800

    补充了一点示例

---
+++ src/config/mysql-config.js
@@ -0,0 +1,14 @@
+/**
+ * @export {
+ *     user: 数据库用户
+ *     password:密码
+ *     database:数据库名称
+ *     connectionLimit:连接池大小
+ * }
+ * */
+export default {
+    user: 'root',
+    password: '123456',
+    database: 'koa2',
+    connectionLimit: 10
+}
---
+++ src/controller/sql.js
@@ -0,0 +1,60 @@
+export default [
+    {
+        routerModule: 'post',
+        url: '/sql/crud',
+        async selectinit(ctx) {
+            try {
+                let result = null
+                let body = {
+                    code: '200',
+                    msg: '查询成功',
+                    data: null
+                }
+                const { crudtyle } = ctx.request.body
+
+                if (crudtyle === 0) {
+                    result = await ctx.$crud.select('user')
+                    body.data = result
+                }
+
+                if (crudtyle === 1) {
+                    const { selectWhere } = ctx.request.body
+                    result = await ctx.$crud.selectWhere('user', selectWhere)
+                    body.data = result
+                }
+
+                if (crudtyle === 2) {
+                    const { update } = ctx.request.body
+                    const where = { id: update.id }
+                    const data = {
+                        zh: update.zh,
+                        mm: update.mm,
+                        name: update.name
+                    }
+                    result = await ctx.$crud.update('user', data, where)
+                    body.data = result
+                    body.msg = '修改成功'
+                }
+
+                if (crudtyle === 3) {
+                    const { add } = ctx.request.body
+                    await ctx.$crud.insert('user', add)
+                    //自定义sql
+                    result = await ctx.$query(`select * from user order by id desc limit 1`)
+                    body.data = result
+                    body.msg = '添加成功'
+                }
+
+                if (crudtyle === 4) {
+                    result = await ctx.$crud.del('user', { id: ctx.request.body.del })
+                    body.data = result
+                    body.msg = '删除成功'
+                }
+
+                ctx.body = body
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    }
+]
