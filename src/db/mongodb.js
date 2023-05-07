5f5cc3407e744f9fd51cb06bd79d27d138f282c9
Git-zaizai <2394276262@qq.com>
2023-05-08 00:01:34 +0800

    额 改了一下更好看的 强迫症 0.0

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -6,13 +6,13 @@ export default async function mongodb() {
 	try {
 		await mongoClient.connect()
 		const db = mongoClient.db(MONGDB_CONFIG.database)
-		console.log('mongodb连接成功')
+		console.log('mongodb 连接 ===> 成功')
 		return {
 			client: mongoClient,
 			db,
 		}
 	} catch (e) {
-		console.log('mongodb连接失败', e)
+		console.log('mongodb 连接 ===> 失败')
 		// return Promise.reject(e)
 	}
 }
--- src/db/mysql.js
+++ src/db/mysql.js
@@ -8,14 +8,19 @@ export const MYSQL_NODE = mysql.createPool(MYSQL_CONFIG)
  * 有问题请在下方输出错误问题
  * */
 export async function mysqlTest() {
-    try {
-        MYSQL_NODE.getConnection((e) => {
-            if (e) {
-                console.log('MySql 连接 ===> 失败')
-            } else {
-                console.log('MySql 连接 ===> 成功')
+
+    const testMySQL = () => new Promise((resolve, reject) => {
+        MYSQL_NODE.connect(err => {
+            if (err){
+                reject('MySql 连接 ===> 失败')
+                return
             }
+            resolve('MySql 连接 ===> 成功')
         })
+    })
+    try {
+        const result = await testMySQL()
+        console.log(result)
     } catch (e) {
         console.log('MySql 连接 ===> 失败')
     }
@@ -23,17 +28,17 @@ export async function mysqlTest() {
 
 export default function query(sql, setup = []) {
     return new Promise((resolve, reject) => {
-        MYSQL_NODE.getConnection(function (Connectionerr, conn) {
+        MYSQL_NODE.getConnection(function (err, conn) {
             conn.query(sql, setup, function (err, result) {
                 if (err) {
                     console.log('query错误', err)
-                    conn.release() //数据查询失败后归还连接
+                    conn.destroy() //数据查询失败后归还连接
                     reject(err)
                     return
                 }
                 resolve(result)
                 setTimeout(() => {
-                    conn.release() //数据查询成功后归还连接
+                    conn.destroy() //数据查询成功后归还连接
                 }, 500)
             })
         })
